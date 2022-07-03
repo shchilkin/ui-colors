33dbd5c7fc3f396b214b929aab6bbfa4b3c0fa68
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2022-07-03 19:01:40 +0300

    feat: implement global store

--- components/ColorInput/ColorInput.tsx
+++ components/ColorInput/ColorInput.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { InputBase, Paper, Typography, Box } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { changeColor } from "../../features/colorManipulation";
+import { RootState } from "../../pages/_app";
 
 // TODO: Add logic
 // TODO: Color validation
 // TODO: Different color modes - RGB, HSL, HSB, Hex, HTML Color Names
-export const ColorInput: React.FunctionComponent = () => (
-    <Box>
-        <Typography>Enter color code:</Typography>
-        <Paper
-            variant="outlined"
-            component="form"
-            sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
-        >
-            <InputBase placeholder="#FFF" />
-        </Paper>
-    </Box>
-);
+export const ColorInput: React.FunctionComponent = () => {
+    const mainColor = useSelector<RootState, string>((state) => state.colorManipulation.mainColor);
+    // const dispatch = useDispatch();
+    return (
+        <Box>
+            <Typography>Enter color code:</Typography>
+            <Typography>Main color from global store: {mainColor}</Typography>
+            <Paper
+                variant="outlined"
+                component="form"
+                sx={{ p: "2px 4px", display: "flex", alignItems: "center", width: 400 }}
+            >
+                <InputBase placeholder="#f0f0f0" />
+            </Paper>
+        </Box>
+    );
+};
